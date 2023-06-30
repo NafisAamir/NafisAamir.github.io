8ca14070bb87653649142bac566881a7901fe897
NafisAamir <aamirnafis82@gmail.com>
2023-06-30 09:43:25 +0530

    Portfoliodeployment3

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App=()=>{
         <About/>
         <Experience/>
         <Projects/>
-        {/* <Testimonial/> */}
+        <Testimonial/>
         <Contact/>
         {/* <Footer/> */}
         </>
